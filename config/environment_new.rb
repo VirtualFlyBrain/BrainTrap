8bfce09ffc3237af5d939a46b9e3873271f68738
Rob Court <robbie.court@gmail.com>
2024-11-22 10:50:27 +0000

    defining directly

--- config/environment_new.rb
+++ config/environment_new.rb
@@ -5,7 +5,8 @@ DATA_ACCESS_HASH = true
 require_relative "application"
 
 # Disable host checking
-config.hosts = nil 
+# config.hosts = nil 
+config.hosts << "braintrap.virtualflybrain.org"
 
 # Initialize the Rails application.
 Rails.application.initialize!
