fdf6c94cfa72f83d8fd08c69eb4ea202b8eaae89
Seymour Knowles-Barley <bt+cmor@users.noreply.github.com>
2024-11-28 22:10:04 +1300

    Updating data image folder to address cross-site issue.

--- config/environment_new.rb
+++ config/environment_new.rb
@@ -1,4 +1,4 @@
-DATA_IMAGE_FOLDER = 'https://virtualflybrain.org/data/BrainTrap/site-images90/'
+DATA_IMAGE_FOLDER = '/data/BrainTrap/site-images90/'
 DATA_ACCESS_HASH = true
 
 # Load the Rails application.
